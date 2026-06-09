0c41713031961ec71fb711c0a6ee8d9938f0664d
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-06-09 16:34:37 +0500

    done

---
+++ class 4/main.py
@@ -0,0 +1,38 @@
+# if    agar
+
+# for example condition
+
+# marks  > 90
+
+##   A grade
+
+# marks > 80
+# B grade
+
+# makrs > 70
+# c grade
+
+# marks > 60 
+# d +grade
+
+
+# hamesha if sirf True par chalega (jab condition True hogi)
+# marks = 70
+# if marks > 50:
+#     print("PASS")
+
+
+
+
+## "if" is a keyword
+
+# marksObtained = 90
+
+# if marksObtained > 80:
+#     print("A+ Grade")
+
+
+# syntax    "likhne ka tareka"
+
+# if condition:
+#     // code
