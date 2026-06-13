3dcfee9f752ed0d47b1d4ec28bb9351396abc75d
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-06-13 16:49:28 +0500

    done

---
+++ class 6/main.js
@@ -0,0 +1,25 @@
+// if(condition){
+//     // code
+// }
+
+
+// JS Syntax
+
+// let num = 6
+
+// if(num % 2 == 0){
+//     console.log("Even")
+// }else{
+//     console.log("Odd")
+// }
+
+
+
+
+let num = 26
+
+if(num % 2  == 0){
+    console.log("Even")
+}else{
+    console.log("Odd")
+}
---
+++ class 6/main.py
@@ -0,0 +1,21 @@
+# num = 8
+
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# for is a keyword
+
+# for num in range(5):
+#     print(num * 3)
+
+
+# input                                 
+
+num = 6
+
+
+num2 = input("Enter your num: ")
+
+print(num2)
